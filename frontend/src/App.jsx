8d232352b02e7d01f7f1c4d4951fad6eaffa41f9
onyourjie <filzahmufidah@student.ub.ac.id>
2025-09-29 02:15:33 +0700

    feat: :sparkles: slicing chart

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import Homepage from "./components/Homepage"
 import Register from "./components/Register"
 import Login from "./components/Login"
 import Landingpage from "./components/Landingpage"
+import Premium from "./components/Premium"
+import Chart from "./components/chart"
 
 export default function App() {
   return (
@@ -13,6 +15,8 @@ export default function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/homepage" element={<Homepage />} />
+        <Route path="/premium" element={<Premium />} />
+        <Route path="/chart" element={<Chart />} />
       </Routes>
     </BrowserRouter>
   )

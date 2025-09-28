116b545c370bac7e536f4ebf670cf0afbc944745
onyourjie <filzahmufidah@student.ub.ac.id>
2025-09-29 00:44:55 +0700

    feat: :art: slicing homepage after auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Homepage from "./components/Homepage"
 import Register from "./components/Register"
+import Login from "./components/Login"
+import Landingpage from "./components/Landingpage"
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route path="/" element={<Landingpage />} />
+        <Route path="/landingpage" element={<Landingpage />} />
         <Route path="/register" element={<Register />} />
-        {/* Optional placeholder until you build it */}
-        <Route path="/login" element={<div className="p-8">Login Page</div>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/homepage" element={<Homepage />} />
       </Routes>
     </BrowserRouter>
   )
